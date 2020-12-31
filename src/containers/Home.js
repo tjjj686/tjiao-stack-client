6b2ec9d376b5026bf6ee8531e47d35e01f6b65d3
Tong Jiao <tjiao@uw.edu>
2020-12-30 22:49:57 -0800

    Adding a redirects file

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="lander">
-        <h1>Scratch</h1>
-        <p className="text-muted">A simple note taking app</p>
+        <h1>Welcome</h1>
+        <p className="text-muted">This is a simple Login and Registration System</p>
       </div>
     </div>
   );
